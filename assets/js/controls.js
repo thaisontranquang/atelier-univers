71caab9440824963b97021b4ceaeb0557cf20660
Thai-Son-TQ <thai-son.tran-quang@mmibordeaux.com>
2025-01-22 16:21:39 +0100

    feat: continue project

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -4,11 +4,32 @@ window.addEventListener('load', event => {
   let stopButton = document.getElementById('stopButton')
   let resetButton = document.getElementById('resetButton')
 
+  let audio = null
+  let whiteNoise = new Audio('/assets/sounds/bruit-blanc.mp3')
+
   startButton.addEventListener('click', function () {
     document.getElementById('startScreen').classList.add('hidden')
+    document.getElementById('startScreen').remove()
+    document.getElementById('colorsContainer').classList.remove('hidden')
+    document.getElementById('globalControlsContainer').classList.remove('hidden')
+    canvas.classList.add('canvas-black')
+  })
+
+  stopButton.addEventListener('click', function () {
+    document.getElementById('controlsContainer').classList.add('hidden')
+    canvas.classList.add('hidden')
+
+    if (audio) {
+      audio.pause()
+      audio.currentTime = 0
+    }
+
+    whiteNoise.currentTime = 0
+    whiteNoise.play()
+
     setTimeout(() => {
-      document.getElementById('startScreen').remove()
-    }, '1000')
+      canvas.remove()
+    }, 1000)
   })
 
   let colorButtons = document.querySelectorAll('.color-button')
@@ -24,6 +45,20 @@ window.addEventListener('load', event => {
 
       let colorClass = 'canvas-' + button.id.replace('Button', '').toLowerCase()
       canvas.classList.add(colorClass)
+
+      whiteNoise.pause()
+      whiteNoise.currentTime = 0
+
+      let color = button.id.replace('Button', '').toLowerCase()
+      let soundPath = `/assets/sounds/${color}-8D.mp3`
+
+      if (audio) {
+        audio.pause()
+        audio.currentTime = 0
+      }
+
+      audio = new Audio(soundPath)
+      audio.play()
     })
   })
 })
